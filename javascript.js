cc4211debc3dfc75af7e5426a1eef360267982b2
meatballmaximus <52974547+meatballmaximus@users.noreply.github.com>
2021-03-08 11:53:02 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
-function myFunction() {
+const numberOfDice = document.getElementById("dice")
+
+
+function roll() {
   var x = Math.floor((Math.random() * 6) + 1);
   document.getElementById("demo").innerHTML = x;
 }
 
+function numberOfRolls() {
+	
+}
